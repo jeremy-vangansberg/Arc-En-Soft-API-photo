8333c8c7b6a0c1b2b88f70ce7f3b35738c9e1cc4
jeremy vgb <jeremy.vangansberg@gmail.com>
2024-12-11 11:58:05 +0100

    save linux

--- api/tasks.py
+++ api/tasks.py
@@ -10,6 +10,5 @@ def process_and_upload_task(template_url, image_url, result_file, xs, ys, rs, ws
     process_and_upload(template_url, image_url, result_file, xs, ys, rs, ws, cs, dhs, dbs, ts, tfs, tcs, tts, txs, tys, ftp_host, ftp_username, ftp_password, params)
 
 @shared_task
-
 def process_intercalaire_task(background_color, width, height, text_blocks, ftp_host, ftp_username, ftp_password):
     return process_intercalaire(background_color, width, height, text_blocks, ftp_host, ftp_username, ftp_password)
